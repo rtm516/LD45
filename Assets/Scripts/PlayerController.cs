ff07bc2fb314c02648e16fb92fd74a3098cc444a
rtm516 <ryantmilner@hotmail.co.uk>
2019-10-05 13:03:21 +0100

    Improved jumping and collision detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	[SerializeField]
-	Transform cameraTransform;
+	[Header("Movement")]
 
 	[SerializeField]
 	float movementSpeed = 1.0f;
@@ -13,15 +12,37 @@ public class PlayerController : MonoBehaviour
 	[SerializeField]
 	float jumpPower = 1.0f;
 
+
+	[Header("Ground detection")]
+
+	[SerializeField]
+	LayerMask groundLayers;
+
+	[SerializeField]
+	Transform topLeft;
+
+	[SerializeField]
+	Transform bottomRight;
+
+
+	[Header("Other")]
+
+	[SerializeField]
+	Transform cameraTransform;
+
 	Rigidbody2D rb;
 
 	bool isGrounded = false;
+
+	int layerMask;
 
 	// Start is called before the first frame update
 	void Start()
     {
 		rb = GetComponent<Rigidbody2D>();
-    }
+
+		layerMask =~ LayerMask.NameToLayer("Ground");
+	}
 
 	// Update is called once per frame
 	void Update()
@@ -43,6 +64,17 @@ public class PlayerController : MonoBehaviour
 
 		rb.velocity = movement;
 
+		isGrounded = Physics2D.OverlapArea(topLeft.position, bottomRight.position, groundLayers);
+
+		/*if (Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.down), 0.3f, layerMask))
+		{
+			isGrounded = true;
+		}
+		else
+		{
+			isGrounded = false;
+		}*/
+
 
 		if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
 		{
@@ -50,7 +82,7 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
-	void OnCollisionEnter2D(Collision2D theCollision)
+	/*void OnCollisionEnter2D(Collision2D theCollision)
 	{
 		if (theCollision.gameObject.tag == "Ground")
 		{
@@ -64,11 +96,28 @@ public class PlayerController : MonoBehaviour
 		{
 			isGrounded = false;
 		}
-	}
+	}*/
 
 	// Player went offscreen
 	void OnBecameInvisible()
 	{
 		GameManager.Instance.EndGame();
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Vector3 topRight = topLeft.position;
+		topRight.x = bottomRight.position.x;
+
+		Vector3 bottomLeft = topLeft.position;
+		bottomLeft.y = bottomRight.position.y;
+
+		Gizmos.color = Color.red;
+
+		Gizmos.DrawLine(topLeft.position, topRight);
+		Gizmos.DrawLine(topRight, bottomRight.position);
+
+		Gizmos.DrawLine(topLeft.position, bottomLeft);
+		Gizmos.DrawLine(bottomLeft, bottomRight.position);
 	}
 }
